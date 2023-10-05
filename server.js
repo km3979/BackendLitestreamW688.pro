43fdc4abdfb264b630f77cce03330c6c41518c94
km3979 <108575443+km3979@users.noreply.github.com>
2023-10-06 01:43:26 +0800

    Update server.js

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const { createProxyMiddleware } = require("http-proxy-middleware");
+const btoa = require("btoa-lite");
 
 const app = express();
 const port = process.env.PORT || 5000;
@@ -14,8 +15,19 @@ app.use((req, res, next) => {
   next();
 });
 
+// Địa chỉ máy chủ backend API của bạn (GitHub không phải là máy chủ backend)
+const backendTarget = "https://backend-litestream-w688-pro.vercel.app/"; // Thay đổi thành địa chỉ thực tế của bạn
+
 // Địa chỉ máy chủ video (được proxy đến)
 const videoTarget = "https://boc8.fun";
+
+// Middleware proxy cho định tuyến "/api" tới máy chủ backend API
+const apiProxy = createProxyMiddleware("/api", {
+  target: backendTarget,
+  changeOrigin: true,
+});
+
+app.use("/api", apiProxy);
 
 // Middleware proxy cho định tuyến "/getVideo" để ẩn địa chỉ URL
 const videoProxy = createProxyMiddleware("/getVideo", {
@@ -29,13 +41,14 @@ const videoProxy = createProxyMiddleware("/getVideo", {
 
 app.use("/getVideo", videoProxy);
 
-function handleProxyError(err, req, res) {
-  res.writeHead(500, {
-    "Content-Type": "text/plain",
-  });
-  res.end(`Error occurred: ${err}`);
-}
+// Endpoint để lấy URL video được mã hóa
+app.get("/getEncodedURL", (req, res) => {
+  const videoURL = `${backendTarget}/getVideo?v=1696501051387`;
+  const encodedURL = btoa(videoURL);
+  res.header("Content-Type", "text/plain");
+  res.send(encodedURL);
+});
 
-app.listen(port, "0.0.0.0", () => {
+app.listen(port, '0.0.0.0', () => {
   console.log(`Proxy Server is running on port ${port}`);
 });
