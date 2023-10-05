22d35dd114e524d986b3635abd5c0a49dae6b585
km3979 <108575443+km3979@users.noreply.github.com>
2023-10-06 02:50:52 +0800

    Create index.js

---
+++ index.js
@@ -0,0 +1,61 @@
+const express = require("express");
+const { createProxyMiddleware } = require("http-proxy-middleware");
+const btoa = require("btoa-lite");
+
+const app = express();
+const port = process.env.PORT || 5000;
+
+// Middleware xử lý CORS
+app.use((req, res, next) => {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header(
+    "Access-Control-Allow-Headers",
+    "Origin, X-Requested-With, Content-Type, Accept"
+  );
+  next();
+});
+
+// Địa chỉ máy chủ backend API của bạn
+const backendTarget = "https://backend-litestream-w688-pro.vercel.app";
+
+// Địa chỉ máy chủ video (được proxy đến)
+const videoTarget = "https://boc8.fun";
+
+// Middleware proxy cho định tuyến "/api" tới máy chủ backend API
+const apiProxy = createProxyMiddleware("/api", {
+  target: backendTarget,
+  changeOrigin: true,
+});
+
+app.use("/api", apiProxy);
+
+// Middleware proxy cho định tuyến "/getVideo" để ẩn địa chỉ URL
+const videoProxy = createProxyMiddleware("/getVideo", {
+  target: videoTarget,
+  changeOrigin: true,
+  pathRewrite: {
+    "^/getVideo": "/kubet", // Thay đổi '/getVideo' thành '/kubet'
+  },
+  onError: handleProxyError, // Sự kiện lỗi cho proxy video
+});
+
+app.use("/getVideo", videoProxy);
+
+function handleProxyError(err, req, res) {
+  res.writeHead(500, {
+    "Content-Type": "text/plain",
+  });
+  res.end(`Error occurred: ${err}`);
+}
+
+// Endpoint để lấy URL video được mã hóa
+app.get("/getEncodedURL", (req, res) => {
+  const videoURL = `${backendTarget}/getVideo?v=1696501051387`;
+  const encodedURL = btoa(videoURL);
+  res.header("Content-Type", "text/plain");
+  res.send(encodedURL);
+});
+
+app.listen(port, () => {
+  console.log(`Proxy Server is running on port ${port}`);
+});
